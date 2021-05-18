cbe966ca367eb0e7210b68b98d9011076cef97eb
Roi malka <roimalka10@gmail.com>
2021-05-18 23:47:44 +0300

    working buttons for nav bar and redux for cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,82 @@
 import styled from "styled-components";
 import React from "react";
+import { useState } from "react";
 import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const [BurgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
+
   return (
     <Container>
       <a>
         <img src="/images/logo.svg" alt="" />
       </a>
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
+        {cars &&
+          cars.map((car, index) => (
+            <a key={index} href="#">
+              {car}
+            </a>
+          ))}
         <a href="#">Solar Roof</a>
         <a href="#">Solar Panels</a>
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu />
+        <CustomMenu onClick={() => setBurgerStatus(true)} />
       </RightMenu>
+      <BurgerNav show={BurgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
+        <li>
+          <a href="">Existing Inventroy</a>
+        </li>
+        <li>
+          <a href="">Use Inventroy</a>
+        </li>
+        <li>
+          <a href="">Trade-in</a>
+        </li>
+        <li>
+          <a href="">Cybertruck</a>
+        </li>
+        <li>
+          <a href="">Roadster</a>
+        </li>
+        <li>
+          <a href="">Semi</a>
+        </li>
+        <li>
+          <a href="">Charging</a>
+        </li>
+        <li>
+          <a href="">Powerwall</a>
+        </li>
+        <li>
+          <a href="">Commercial Energy</a>
+        </li>
+        <li>
+          <a href="">Utiities</a>
+        </li>
+        <li>
+          <a href="">Test Drive</a>
+        </li>
+        <li>
+          <a href="">Find Us</a>
+        </li>
+        <li>
+          <a href="">Support</a>
+        </li>
+        <li>
+          <a href="">United States</a>
+        </li>
+      </BurgerNav>
     </Container>
   );
 };
@@ -35,6 +91,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
   a {
     font-size: 16px;
 
@@ -72,6 +129,40 @@ const RightMenu = styled.div`
 `;
 
 const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: white;
+  width: 295px;
+  z-index: 20;
+  list-style: none;
+  padding: 20px;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.2s;
+  li {
+    padding: 20px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const CustomClose = styled(CloseIcon)`
   cursor: pointer;
 `;
 
